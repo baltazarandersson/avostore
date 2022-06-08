4b82e78e9bd8c2d7c9ef54d95c19a123de7362f9
Baltazar <balti.kpo@gmail.com>
2022-06-08 12:00:50 -0300

    Changes props from server side props to static props and updated API url

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 import ProductWrapper from "@components/ProductWrapper/ProductWrapper";
 
-export const getServerSideProps = async () => {
-  const response = await fetch("http://localhost:3000/api/avo");
+export const getStaticProps = async () => {
+  const response = await fetch("https://avostore-baltazar.vercel.app/api/avo");
   const { data: productList }: TAPIAvoResponse = await response.json();
 
   return {
